6165c850b984d55e88acf535fc4b20a21bf450e3
Ooops <dev.seb.3745@gmail.com>
2025-05-26 21:58:01 -0500

    factura gets

--- backend/src/document/factura/factura.controller.ts
+++ backend/src/document/factura/factura.controller.ts
@@ -1,5 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { DocumentController } from '../document.controller';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { FacturaService } from './factura.service';
 
+@ApiTags('Factura')
 @Controller()
-export class FacturaController extends DocumentController {}
+export class FacturaController {
+  constructor(private readonly facturaService: FacturaService) {}
+
+  @Get()
+  findAll() {
+    return this.facturaService.findAllWithRelations();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.facturaService.findOneWithRelations(id);
+  }
+}
--- backend/src/document/factura/factura.service.ts
+++ backend/src/document/factura/factura.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Factura } from './entities/factura.entity';
+import { LineaFactura } from 'src/line/factura/entities/factura.entity';
+import { Documento } from '../entities/Documento.entity';
 
 @Injectable()
-export class FacturaService {}
+export class FacturaService {
+  constructor(
+    @InjectRepository(Factura)
+    private readonly facturaRepo: Repository<Factura>,
+    @InjectRepository(LineaFactura)
+    private readonly lineaFacturaRepo: Repository<LineaFactura>,
+    @InjectRepository(Documento)
+    private readonly documentoRepo: Repository<Documento>,
+  ) {}
+
+  async findAllWithRelations(): Promise<Factura[]> {
+    return this.facturaRepo.find({
+      relations: {
+        paciente: true,
+        lineas: {
+          producto: true,
+          servicio: true,
+        },
+      },
+      order: { id: 'DESC' },
+    });
+  }
+
+  async findOneWithRelations(id: number): Promise<Factura> {
+    const factura = await this.facturaRepo.findOne({
+      where: { id },
+      relations: {
+        paciente: true,
+        lineas: {
+          producto: true,
+          servicio: true,
+        },
+      },
+    });
+    if (!factura) throw new Error('Factura no encontrada');
+    return factura;
+  }
+}
